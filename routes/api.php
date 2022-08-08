2c476e9ce650f4bd4ee6fbd8d7e8305303a3b36f
Khada4Yeah <pespinoza9494@utm.edu.ec>
2022-08-07 22:27:59 -0500

    Laravel Sanctum

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,16 +21,27 @@ Route::middleware("auth:sanctum")->get("/user", function (Request $request) {
     return $request->user();
 });
 
+Route::group(["middleware" => "auth:sanctum"], function () {
+    Route::resource("post", PostController::class)->except(["create", "edit"]);
+    Route::resource("category", CategoryController::class)->except([
+        "create",
+        "edit",
+    ]);
+});
+
 // Categorias
 Route::get("category/all", [CategoryController::class, "all"]);
 Route::get("category/slug/{slug}", [CategoryController::class, "slug"]);
-Route::resource("category", CategoryController::class)->except([
-    "create",
-    "edit",
-]);
+// Route::resource("category", CategoryController::class)->except([
+//     "create",
+//     "edit",
+// ]);
 Route::get("category/{category}/posts", [CategoryController::class, "posts"]);
 
 // Post
 Route::get("post/all", [PostController::class, "all"]);
 Route::get("post/slug/{slug}", [PostController::class, "slug"]);
-Route::resource("post", PostController::class)->except(["create", "edit"]);
+// Route::resource("post", PostController::class)->except(["create", "edit"]);
+
+// Usuarios
+Route::post("user/login", [UserController::class, "login"]);
